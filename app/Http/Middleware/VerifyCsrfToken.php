14e10ac5ee0ef24717cb71fbe0d59fcc6a1f9c33
Nenad Radulovic <nenadradulovic@Nenads-MacBook-Pro.local>
2020-12-03 13:45:40 +0100

    #LAGU-7 Dodate Rute

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,10 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'testPost',
+        'testPatch',
+        'testPut',
+        'testPatch',
+        'testDelete'
     ];
 }
